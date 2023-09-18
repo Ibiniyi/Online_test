7fe8ccfe2a4ae982458799c3d355276aef1fd1be
Anozie Chibuike <chibuikeanozie0@gmail.com>
2023-09-18 22:39:31 +0100

    new push

--- app/models.py
+++ app/models.py
@@ -8,9 +8,31 @@ class Student(db.Model):
     score = db.Column(db.Integer)
     matric_no = db.Column(db.String(255),unique=True,nullable=False)
     written = db.Column(db.Integer,default=0)
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the object.
+        """
+        attributes = {}
+        for column in self.__table__.columns:
+            attribute_name = column.name
+            attribute_value = getattr(self, attribute_name)
+            attributes[attribute_name] = attribute_value
+        return attributes
 
 class Question(db.Model):
     id = db.Column(db.Integer,primary_key=True,unique=True,nullable=False)
     question = db.Column(db.String(255), nullable=False, unique=True)
     answer = db.Column(db.String(255), nullable=False)
     options = db.Column(db.JSON, nullable=False)
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the object.
+        """
+        attributes = {}
+        for column in self.__table__.columns:
+            attribute_name = column.name
+            attribute_value = getattr(self, attribute_name)
+            attributes[attribute_name] = attribute_value
+        return attributes
+
+    
